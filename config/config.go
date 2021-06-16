2d443d5bfe2307d402f3a09bf2c283c48ea93a93
FA Tulloh <77918734+yourtulloh@users.noreply.github.com>
2021-06-16 23:53:13 +0800

    Update config.go

--- config/config.go
+++ config/config.go
@@ -76,7 +76,7 @@ const (
 {{- end}}{{if .EnableTelegraph}}
 {{.ContentTitle}} <a href="{{.TelegraphURL}}">Telegraph</a> | <a href="{{.RawLink}}">原文</a>
 {{- else }}
-/torrent {{.RawLink}}
+/t {{.RawLink}}
 {{- end }}
 {{.Tags}}
 `
@@ -87,7 +87,7 @@ const (
 {{- end}}{{if .EnableTelegraph}}
 {{.ContentTitle}} [Telegraph]({{.TelegraphURL}}) | [原文]({{.RawLink}})
 {{- else }}
-/torrent {{.RawLink}})
+/t {{.RawLink}})
 {{- end }}
 {{.Tags}}
 `
